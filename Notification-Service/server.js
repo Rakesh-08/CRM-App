297bc32104b1b74cf7fd801a81ac67b0fa2639d8
Rakesh Mandal <rkm8448746347@gmail.com>
2023-05-25 23:25:00 +0530

    notificaton apis for crm

---
+++ Notification-Service/server.js
@@ -0,0 +1,42 @@
+const dbConfig= require("./configs/dbConfig")
+const mongoose= require("mongoose")
+const serverConfig = require("./configs/serverConfig");
+const express = require("express");
+const expressApp = express();
+const bodyParser = require("body-parser");
+ 
+
+expressApp.use(bodyParser.json());
+
+
+
+mongoose.connect(dbConfig.DB_URL);
+
+let db = mongoose.connection;
+
+db.on("error", err => {
+    console.log('Error while connecting to the DB ' + dbConfig.DB_NAME + " " + err.message)
+});
+db.once("open", () => {
+    console.log("connected to the Db " + dbConfig.DB_NAME);
+})
+
+
+// import the routes
+
+require("./routes/ticketNotification")(expressApp);
+
+
+expressApp.listen(serverConfig.PORT, () => {
+    
+    console.log("your application is listening on port : "+ serverConfig.PORT)
+})
+
+
+
+
+
+
+
+
+
