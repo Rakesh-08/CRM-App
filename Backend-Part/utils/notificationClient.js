17ee787b6b78962f325895a9c437a19012bcf006
Rakesh Mandal <87007801+Rakesh-08@users.noreply.github.com>
2023-06-06 10:10:38 +0530

    Update notificationClient.js

--- Backend-Part/utils/notificationClient.js
+++ Backend-Part/utils/notificationClient.js
@@ -15,11 +15,19 @@ module.exports = (ticketId, subject, content, emailIds, requester) => {
         data: reqBody,
         headers:{"Content-Type":"application/json"}
     }
- 
-    client.post("https://notification-service-m4gi.onrender.com/notificationService/api/v1/notification", args, (data,response) => {
+    let baseUrl= process.env.MONGODB_URI ? "https://notification-service-m4gi.onrender.com":"http://localhost:8080"
+    
+    
+    client.post(`${baseUrl}/notificationService/api/v1/notification`, args, (data,response) => {
         
         console.log(data)
     })
+
+ 
+//     client.post("https://notification-service-m4gi.onrender.com/notificationService/api/v1/notification", args, (data,response) => {
+        
+//         console.log(data)
+//     })
 
 
 }
