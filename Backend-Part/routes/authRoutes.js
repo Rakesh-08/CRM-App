ab541c3d4927e2623d3ee172ebdf6042d656cb81
Rakesh Mandal <rkm8448746347@gmail.com>
2023-06-15 23:16:22 +0530

    removing everything except frontend-part

--- Backend-Part/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const authController = require("../controllers/authController");
-const authValidators = require("../middlewares/index")
-
-module.exports = function (app) {
-    app.post("/crm/api/v1/auth/signup", [authValidators.verifySignup], authController.signup)
-    app.post("/crm/api/v1/auth/signin", [], authController.signin)
-}
-
-
