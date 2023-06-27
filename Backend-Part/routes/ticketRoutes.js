dbfdbbfffde99df409220b242016b9eea3513387
Rakesh Mandal <87007801+Rakesh-08@users.noreply.github.com>
2023-06-27 22:25:26 +0530

    Update ticketRoutes.js

--- Backend-Part/routes/ticketRoutes.js
+++ Backend-Part/routes/ticketRoutes.js
@@ -7,7 +7,8 @@ module.exports = function (app) {
     app.post("/crm/api/v1/tickets", [authJwtValidators.verifyToken, ticketValidators.validateTicketBody], ticketController.createTicket)
     app.put("/crm/api/v1/tickets/:_id", [authJwtValidators.verifyToken, ticketValidators.validateTicketStatus], ticketController.updateTicket)
     app.get("/crm/api/v1/tickets", [authJwtValidators.verifyToken], ticketController.getAllTickets)
-    app.get("/crm/api/v1/tickets/:_id", [authJwtValidators.verifyToken], ticketController.getTicketById)
+    app.get("/crm/api/v1/tickets/:_id", [authJwtValidators.verifyToken], ticketController.getTicketById);
+    app.delete("/crm/api/v1/tickets/:_id",[authJwtValidators.verifyToken],ticketController.deleteTicket);
     app.put("/crm/api/v1/assignTickets",[ticketValidators.validateTicketAssignment],ticketController.assignTicketToEngineer)
 
 
