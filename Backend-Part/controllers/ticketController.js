54d376bb8ae2986edabf61e421524a41e6bb868a
Rakesh Mandal <87007801+Rakesh-08@users.noreply.github.com>
2023-07-01 16:48:55 +0530

    Update ticketController.js

--- Backend-Part/controllers/ticketController.js
+++ Backend-Part/controllers/ticketController.js
@@ -11,7 +11,6 @@ const createTicket = async (req, res) => {
         ticketPriority: req.body.ticketPriority,
         description: req.body.description,
         status: req.body.status,
-        comments:req.body.comments,
         reporter: req.userId, // this is coming from authjwt middleware
 
 
